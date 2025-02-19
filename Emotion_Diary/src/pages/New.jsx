c2a8bdc805aa4799d6044de8baba04a57cf523f8
ttangzzi <cvbg0802@gmail.com>
2025-02-19 15:41:28 +0900

    New page UI와 기능

--- Emotion_Diary/src/pages/New.jsx
+++ Emotion_Diary/src/pages/New.jsx
@@ -1,5 +1,25 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from "../App";
+import { useContext } from "react";
+
 const New = () => {
-  return <div>New</div>
-}
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+  };
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
+};
 
 export default New;
